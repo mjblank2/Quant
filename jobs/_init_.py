543ba07dfbe6cf6f8b82af34c90abf863d2c1746
mjblank2 <marques@goblankcapital.com>
2025-10-02 15:55:51 -0500

    Update _init_.py

--- jobs/_init_.py
+++ jobs/_init_.py
@@ -1,1 +1,9 @@
+"""
+jobs package for worker entrypoints.
+
+This package exposes the `worker` module so that process managers (such as
+Render or Celery) can import `jobs.worker` as an entrypoint.  Without an
+``__init__.py`` file, Python would treat the ``jobs`` directory as a
+namespace package and module resolution for `jobs.worker` would fail.
+"""
 
